4753bde5a72b9aa073cdd54c25a2df9826cd5f55
khusheel <khusheel.verma@capgemini.com>
2022-06-03 14:04:26 +0530

    add all

---
+++ figmaDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  figmaDemoApp
+//
+//  Created by CG App Mac on 03/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
